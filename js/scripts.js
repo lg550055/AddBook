50353d48031057bd9bb2007c646a626924fc54b1
lgonz <lg550055@hotmail.com>
2021-10-20 18:32:10 -0500

    updates funcionality to include show individual contact details and contact delete

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,92 @@
+// Business logic for AddressBook
+function AddressBook() {
+  this.contacts = {};
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
+};
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+// Business logic for Contacts
+function Contact(name, surname, email, phone) {
+  this.name = name;
+  this.surname = surname;
+  this.email = email;
+  this.phone = phone;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+// User Interface logic
+let book = new AddressBook();
+
+function contactDetails(book) {
+  let htmlContactInfo = '';
+  Object.keys(book.contacts).forEach(function(key) {
+    const contact = book.findContact(key);
+    htmlContactInfo += '<li class="list-group-item" id='+contact.id +'>'+ contact.name+" "+contact.surname+'</li>';
+  });
+  $('#book').html(htmlContactInfo);
+}
+
+function showContact(id) {
+  const contact = book.findContact(id);
+  $('#contact').show();
+  $('.name').html(contact.name);
+  $('.surname').html(contact.surname);
+  $('.email').html(contact.email);
+  $('.phone').html(contact.phone);
+  let buttons = $('#buttons');
+  buttons.empty();
+  buttons.append('<button class="deleteButton" id='+id+'>Delete</button>')
+}
+
+function contactListner() {
+  $('#book').on('click', 'li', function() {
+    showContact(this.id);
+
+  $('#buttons').on('click', '.deleteButton', function() {
+    book.deleteContact(this.id);
+    $('#contact').hide();
+    contactDetails(book);
+  })
+  });
+}
+
 $(document).ready(function() {
-  let book = [];
+  contactListner();
 
   $("#new").submit(function() {
     event.preventDefault();
-    const person = {name:$('#name').val(), last:$('#last').val(), email:$('#email').val(), phone:$('#phone').val() };
-    book.push( person );
-    $('#book').append( '<li class="list-group-item">' + person.last + ', ' + person.name + '</li>' )
-
+    let contact = new Contact($('#name').val(), $('#last').val(), $('#email').val(), $('#phone').val());
+    book.addContact(contact);
+    contactDetails(book);
+    $('#new').trigger('reset');
   });
 
-  $('.list-group').click(function() {
-    alert( $(this.email) );
-  });
 
 });
